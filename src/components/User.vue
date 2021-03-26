05328113582dda4d0bd9049f09af9f786784fd6d
OmarHegazi94 <edgeofdarkness_x1@hotmail.com>
2021-03-26 21:08:50 +0200

    first commit

---
+++ src/components/User.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <p>User ID is: {{ userID }}</p>
+    <p>User Name is: {{ userData.name }} | Status: {{ userData.status }}</p>
+    <button @click="changeUserID">change id</button>
+    <button @click="changeUserData">change data</button>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <app-user-details
+          :UserID="userID"
+          @update:UserID="userID = $event"
+          :UserData="userData"
+          @update:UserData="userData = $event">
+        </app-user-details>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit></app-user-edit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserDetails from "./UserDetails.vue";
+import UserEdit from "./UserEdit.vue";
+
+export default {
+  data() {
+    return {
+      userID: 0,
+      userData: {
+        name: 'Omar Hegazi',
+        status: 'Unknowen'
+      }
+    }
+  },
+  methods: {
+    changeUserID() {
+      this.userID = 10;
+    },
+    changeUserData() {
+      this.userData.name = 'Mero Gazi'
+      this.userData.status = 'Online'
+    }
+  },
+  components: {
+    appUserDetails: UserDetails,
+    appUserEdit: UserEdit,
+  },
+};
+</script>
+
+<style >
+div {
+  background-color: lightblue;
+}
+</style>
---
+++ src/components/UserDetails.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User ID {{ UserID }}</p>
+    <p>User Name: {{ UserData.name }} | Status: {{ UserData.status }}</p>
+    <button @click="resetID(newID)">Reset ID</button>
+    <button @click="changeUserData(newData)">Change Data</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newID: 50,
+            newData: {
+                name: 'KOKO WAWA',
+                status: 'Offline'
+            }
+        }
+    },
+    props: {
+        UserID: {
+            type: Number,
+            default: 1
+        },
+        UserData: {
+            name: {
+                type: String,
+                default: 'Omar'
+            },
+            status: {
+                type: String,
+                default: 'Unknowen'
+            }
+        }
+    },
+    emits: ['update:UserID', 'update:UserData'],
+    methods: {
+        resetID(newID) {
+            this.$emit('update:UserID', newID)
+        },
+        changeUserData(newData) {
+            this.$emit('update:UserData', newData);
+        }
+    }
+}
+</script>
+
+<style scoped>
+div {
+  background-color: lightcoral;
+}
+</style>
